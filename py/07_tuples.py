5ba34790a3c67840d48a2f09f7fd18984b01b195
diyjml <diyanajamaludin@gmail.com>
2025-11-13 09:53:28 +0800

    tuples

---
+++ py/07_tuples.py
@@ -0,0 +1,16 @@
+# coordinates = (10,20)
+# person = ("Alice", 25, "Engineer")
+# single_item = (42,) #Note the comma for single item 
+
+# Tuple operations
+# print(coordinates[0]) # Accessing elements
+# print(len(person))   # Length of tuple
+
+# Check to see what type it is:
+# x = (10, 20, 30)
+# print(type(x))
+
+# Checking immutability of tuples:
+# groceries = ("oats", "milk", "eggs", "bananas")
+# groceries[0] = "bread"  # This will raise a TypeError since tuples are immutable
+# # print(type(groceries))
