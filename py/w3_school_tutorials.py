17a1e11e82ee711083c10e641b7e131cb0cf2451
diyjml <diyanajamaludin@gmail.com>
2025-11-18 23:15:36 +0800

    w3 revisions

---
+++ py/w3_school_tutorials.py
@@ -0,0 +1,256 @@
+# x = str(3)
+# y = int(3)
+# z = float(3)
+
+# print(x)
+# print(y)
+# print(z)
+
+# x = 5
+# y = "John"
+# print(type(x))
+# print(type(y))
+
+# x, y, z = "apple", "bananas", "cherries"
+# print(x)
+# print(y)
+# print(z)
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# fruits = ["apple", "orange", "bananas"]
+# x, y, z = fruits 
+# print(x)
+# print(y)
+# print(z)
+
+# x = "Python "
+# y = "is "
+# z = "awesome "
+
+# print(x, y, z)
+# print(x + y + z)
+
+# x = 5
+# y = 10
+# print(x + y)
+
+# x = 5 
+# y = "John"
+# print(x, y)
+# print(x + y)
+
+# x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+
+# myfunc()
+
+# print("Python is " + x)
+
+# x = complex(1j)
+# print(x)
+
+# x = dict(name="John", age= 36)
+# print(x)
+
+# x = set(("apple", "banana"))
+# print(x)
+
+# x = 35e3
+# y = 12E4
+# z = -87.7e100
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x = 3+5j
+# y = 5j
+# z = -5j
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# Convert from one type to another 
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+
+# a = float(x) # int to float 
+# b = int(y) # float to int
+# c = complex(x) # int to complex 
+
+# print(a)
+# print(b)
+# print(c)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# import random
+# print(random.randrange(1, 10))
+
+# x = float(2.8)
+# y = float(1)
+# z = float(3)
+# w = float("4.2")
+
+# print(z)
+# print(w)
+
+# print("Hello")
+# print('Hello')
+
+# print("It's alright")
+# print("He is called 'Johnny'")
+# print('He is called "Johnny"')
+
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+
+# a = '''Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua.'''
+# print(a)
+
+# a = "Hello, World!"
+# print(a[1])
+
+# a = "Hello, Diyana."
+# print(a[5])
+
+# For loop 
+# for x in "banana":
+#   print(x)
+
+# Get the length of a string
+# a = "hello, world!"
+# print(len(a))
+
+# txt = "I want a soda."
+# print("soda" in txt)
+
+# txt = "I want a soda!!!"
+# if "soda" in txt:
+#     print("Yes, it is available.")
+
+# txt = "The best things in life are free!"
+# print("expensive" not in txt)
+
+# b = "Hello, World!"
+# print(b[2:9])
+
+# b = "Hello, World!"
+# print(b[:5])
+# print(b[-5])
+# print(b[-5:-2])
+# print(b.upper())
+# print(b.lower())
+# print(b.strip())
+# print(b.replace("H", "J"))
+# print(b.split(","))
+
+# a = "Hello"
+# b = "world"
+# c = a + " "  + b
+# print(c)
+
+# age = 36 
+# txt = f"Ahmad is {age}"
+# print(txt)
+
+# price = 68
+# txt = f"The price for the serum is RM{price}"
+# 
+# txt = f"The price for the serum is RM{20 * 3}"
+# print(txt)
+# print(f"The price for the serum is RM{price}")
+
+# txt = "We are the so-called "Melayu"" #will come out as Error 
+# txt = "We are so-called \"Melayu\""
+# txt = "We are the so-called \"Vikings\" from the north."
+# print(txt)
+
+# text = "hello, world"
+# print(text.capitalize())
+
+# print(10 > 9)
+# print(10 == 9)
+# print (10 < 9)
+
+# a = 37
+# b = 49
+
+# if b > a:
+#     print("b is greater than a")
+# else:
+#     print("b is not greater than a")
+
+# print(bool("Hello"))
+# print(bool(35))
+
+# x = "Hello"
+# y = 35
+
+# print(bool(x))
+# print(bool(y))
+
+# x = 15
+# y = 4
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x % y)
+# print(x ** y)
+# print(x // y)
+
+# number = [1, 2, 3, 4, 5]
+# count = len(number)
+# # if count > 3:
+# #     print(f"List has {count} elements")
+
+# if (count:= len(number)) > 3:
+#     print(f"List has {count} elements")
+
+# x = 5
+
+# print(1 < x < 10)
+
+# print(1 < x and x < 10)
+
+# fruits = ["bananas", "apples", "cherries"]
+# print("apples" in fruits)
+
+# text = "Hello, world!"
+# print("H" in text)
+# print("world" in text)
+# print("z" not in text)
+
+# print((6 + 3) - (6 + 3))
+# print(100 + 5 * 3)
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[-1])
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[-1])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
